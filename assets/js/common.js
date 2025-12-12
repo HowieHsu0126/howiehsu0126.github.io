d440582ef48337ff27c3af7d4824f7fd5e72ea92
Haowei <1311304948@qq.com>
2025-12-12 15:36:29 +0800

    Homepage

--- assets/js/common.js
+++ assets/js/common.js
@@ -30,10 +30,38 @@ $(function () {
         (event.key === 'Escape') && closeLightbox()
     })
     
+    const wrapParentheses = (text) => {
+        const result = []
+        let i = 0
+        while (i < text.length) {
+            if (text[i] === '(') {
+                let depth = 1
+                let start = i
+                i++
+                while (i < text.length && depth > 0) {
+                    if (text[i] === '(') depth++
+                    else if (text[i] === ')') depth--
+                    i++
+                }
+                if (depth === 0) {
+                    const content = text.substring(start, i)
+                    result.push('<span class="text-parentheses">' + content + '</span>')
+                } else {
+                    result.push(text[start])
+                    i = start + 1
+                }
+            } else {
+                result.push(text[i])
+                i++
+            }
+        }
+        return result.join('')
+    }
+    
     $('.news-timeline .timeline-title').each(function() {
         let html = $(this).html()
         
-        html = html.replace(/\(([^)]+)\)/g, '<span class="text-parentheses">($1)</span>')
+        html = wrapParentheses(html)
         
         html = html.replace(/Top 5%/g, '<span class="award-top5">Top 5%</span>')
         html = html.replace(/Silver Medal/g, '<span class="award-silver">Silver Medal</span>')
